eebc52bf70291c712d2b81cc69a5dff0d096f0e0
DenisBardashevich <ackapbinka@mail.ru>
2025-03-24 20:09:17 +0300

    f

--- headerSlider.js
+++ headerSlider.js
@@ -1,7 +1,7 @@
 let currentSlide3 = 0;
 
 function showSlide2(index) {
-    const slides3 = document.querySelectorAll('.RummyZion-info-slide');
+    const slides3 = document.querySelectorAll('.RummyTourneyClub-info-slide');
     const totalSlides3 = slides3.length;
 
     if (index >= totalSlides3) {
@@ -12,7 +12,7 @@ function showSlide2(index) {
         currentSlide3 = index;
     }
 
-    const slider3 = document.querySelector('.RummyZion-info-slider');
+    const slider3 = document.querySelector('.RummyTourneyClub-info-slider');
     slider3.style.transform = `translateX(-${currentSlide3 * 100}%)`;
 }
 
@@ -21,8 +21,8 @@ function changeSlide2(direction) {
 }
 
 // Add event listeners to the buttons
-document.querySelector('.RummyZion-info-prev').addEventListener('click', () => changeSlide2(-1));
-document.querySelector('.RummyZion-info-next').addEventListener('click', () => changeSlide2(1));
+document.querySelector('.RummyTourneyClub-info-prev').addEventListener('click', () => changeSlide2(-1));
+document.querySelector('.RummyTourneyClub-info-next').addEventListener('click', () => changeSlide2(1));
 
 // Show the first slide on page load
 showSlide2(currentSlide3);
